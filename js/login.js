e1c9ffa7bd124c576e403902005db8c0268e7cd7
Pablo <contact@fastweb.com>
2021-04-03 23:48:57 -0300

    [WIP] Add changes in project files

---
+++ js/login.js
@@ -0,0 +1,117 @@
+testForm(2, 'log in');
+
+
+//TODO: The following functions should be imported from "testFunctions.js" and instead of being defined here, but i don´t know how to do it.
+function testForm(expectedInputs, expectedButtonText) {
+
+    var error = document.getElementById('error');
+    var errorResults = [];
+    var fullMessage = '';
+
+
+    var testCheckFormResult = testCheckForm();
+    if (testCheckFormResult != undefined) {
+        errorResults.push(testCheckFormResult);
+    }
+
+    var testInputsLengthResult = testInputsLength(expectedInputs);
+    if (testInputsLengthResult != undefined) {
+        errorResults.push(testInputsLengthResult);
+    }
+
+    var testCheckInputsWithLabelsResult = testCheckInputsWithLabels();
+    if (testCheckInputsWithLabelsResult != undefined) {
+        errorResults.push(testCheckInputsWithLabelsResult);
+    }
+
+    var testCheckRequiredFieldsResult = testCheckRequiredFields();
+    if (testCheckRequiredFieldsResult != undefined) {
+        errorResults.push(testCheckRequiredFieldsResult);
+    }
+
+    var testCheckButtonContentResult = testCheckButtonContent(expectedButtonText);
+    if (testCheckButtonContentResult != undefined) {
+        errorResults.push(testCheckButtonContentResult);
+    }
+
+    if (errorResults.length != 0) {
+        fullMessage = 'Validation results: ' + errorResults.join(', ');
+    } else {
+        fullMessage = 'Validation results: every validation has passed.';
+    }
+
+    error.innerHTML = fullMessage;
+};
+
+function testCheckForm() {
+    var form = document.getElementsByTagName('form');
+
+    if (form.length == 0) {
+        var errorMessage = 'Form is not found';
+        return errorMessage;
+    }
+};
+
+function testInputsLength(expectedTotal) {
+    var inputs = document.getElementsByTagName('input');
+
+    if (inputs.length != expectedTotal) {
+        var errorMessage = 'There are ' + inputs.length + ' inputs, but expected ' + expectedTotal;
+        return errorMessage;
+    }
+};
+
+function testCheckInputsWithLabels() {
+    var inputs = document.getElementsByTagName('input');
+    var labels = document.getElementsByTagName('label');
+    var count = 0;
+
+    if (inputs.length > labels.length) {
+        var errorMessage = 'There are missing labels for inputs';
+        return errorMessage;
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+        var id = inputs[i].getAttribute('id');
+
+        for (var j = 0; j < labels.length; j++) {
+            var forValue = labels[j].getAttribute('for');
+            if (id === forValue) {
+                count++;
+                break;
+            }
+        }  
+    }
+
+    if (count != inputs.length){
+        var errorMessage = 'There are missing labels for inputs';
+        return errorMessage;
+    }
+};
+
+function testCheckRequiredFields(){
+    var inputs = document.getElementsByTagName('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (!inputs[i].required) {
+            var errorMessage = 'there are optional inputs';
+            return errorMessage;
+        }
+    }
+};
+
+function testCheckButtonContent(expectedContent){
+    var buttonSubmit = document.getElementsByTagName('button').submit;
+
+    if (buttonSubmit === undefined) {
+        var errorMessage = 'missing submit button';
+        return errorMessage;
+    } 
+
+    var buttonSubmitContent = buttonSubmit.textContent;
+    
+    if (buttonSubmitContent != expectedContent) {
+        var errorMessage = 'button content is invalid';
+        return errorMessage;
+    }
+};
